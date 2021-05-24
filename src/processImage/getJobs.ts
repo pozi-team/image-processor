533c2a5794e9b318319dbd2c89fdb0db4ce1e987
Rizky <spondbob@eamca.com>
2021-05-24 11:47:15 +1000

    fix: handle height in filename

--- src/processImage/getJobs.ts
+++ src/processImage/getJobs.ts
@@ -1,11 +1,13 @@
 export default ({ dimensions, fileName, tempLocalPath }) => {
   const { height, width } = dimensions.imageUrl
-  const mainDimension = `${width}x${height}`
+  const mainDimension = `${width}${hasHeight(height)}`
   return Object.keys(dimensions).map(key => {
     const size          = dimensions[ key ]
-    const newDimension  = `${size.width}x${size.height}`
+    const newDimension  = `${size.width}${hasHeight(size.height)}`
     const name          = fileName.replace(mainDimension, newDimension)
     const srcPath       = tempLocalPath.replace(mainDimension, newDimension)
     return { name, size, srcPath }
   })
 }
+
+const hasHeight = (height) => height ? `x${height}` : ''
