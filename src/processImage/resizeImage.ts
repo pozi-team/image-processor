f5254ca203fd7a9efcebfd55fa168f713eedf857
Rizky <spondbob@eamca.com>
2022-05-05 11:22:24 +1000

    feat: force square image

--- src/processImage/resizeImage.ts
+++ src/processImage/resizeImage.ts
@@ -1,14 +1,20 @@
 import sharp from 'sharp'
 
+const getDimensions = (size:any, metadata:sharp.Metadata) => {
+  if ((size.width > 0 || size.height > 0) && size.width === size.height) return size
+  const width = metadata.width! <= metadata.height! ? size.width : null
+  const height = metadata.height! <= metadata.width! ? size.height : null
+  return {height, width}
+}
+
 export default async ({ buffer, size, srcPath }) => {
   try {  
     const image = sharp(buffer)
     await image.metadata().then(metadata => {
-      const w = metadata.width! <= metadata.height! ? size.width : null
-      const h = metadata.height! <= metadata.width! ? size.height : null
+      const {height, width} = getDimensions(size, metadata)
 
       return image
-        .resize(w, h, { kernel: sharp.kernel.lanczos3, withoutEnlargement: true })
+        .resize(width, height, { kernel: sharp.kernel.lanczos3, withoutEnlargement: true })
         .toFormat(metadata.format!)
         .toFile(srcPath)
     })
